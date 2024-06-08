7db04bf1dd2ac69206173c9937b19655cf22496a
Edward Baitsewe <edward@monatemedia.com>
2024-06-09 00:12:27 +0200

    Environment and project setup

---
+++ app/Models/UsedFeature.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UsedFeature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'feature_id',
+        'user_id',
+        'credits'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'data' => 'array',
+        ];
+    }
+
+    //Relationship to User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //Relationship to Feature
+    public function feature()
+    {
+        return $this->belongsTo(Feature::class);
+    }
+}
