29386c42c6b4dc57a5d772336e4fd149d610c065
Edward Baitsewe <edward@monatemedia.com>
2024-06-09 03:39:41 +0200

    Set up Migrations, Models and Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Package;
 use App\Models\User;
+use App\Models\Feature;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +20,43 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        Feature::create([
+            'image' => 'https://static-00.iconduck.com/assets.00/plus-icon-2048x2048-z6v59bd6.png',
+            'route_name' => 'feature1.index',
+            'name' => 'Calculate Sum',
+            'description' => 'Calculate sum of two numbers',
+            'required_credits' => 1,
+            'active' => true,
+        ]);
+
+        Feature::create([
+            'image' => 'https://cdn-icons-png.freepik.com/512/929/929430.png',
+            'route_name' => 'feature2.index',
+            'name' => 'Calculate Difference',
+            'description' => 'Calculate difference of two numbers',
+            'required_credits' => 3,
+            'active' => true,
+        ]);
+
+        Package::create([
+            'name' => 'Basic',
+            'price' => 5,
+            'credits' => 20
+        ]);
+
+        Package::create([
+            'name' => 'Silver',
+            'price' => 20,
+            'credits' => 100
+        ]);
+
+        Package::create([
+            'name' => 'Gold',
+            'price' => 50,
+            'credits' => 500
         ]);
     }
 }
